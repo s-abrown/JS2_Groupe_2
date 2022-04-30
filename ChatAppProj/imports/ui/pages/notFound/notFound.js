93f2a61a4702bcbd0f90ec38312882e53f238e01
Sophie Brown <sophie.abrown@hotmail.com>
2022-04-30 13:05:49 +0200

    first vers of 404 page but want to center it better

--- ChatAppProj/imports/ui/pages/notFound/notFound.js
+++ ChatAppProj/imports/ui/pages/notFound/notFound.js
@@ -1,1 +1,16 @@
-import './notFound.html';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import './notFound.html'
+
+// no relevant collection not relevant subscriptions no relevant helpers for this page. 
+
+// Listener/events:
+Template.notFound.events({
+    //sends you back to the home page upon clicking the "back" div
+    'click .text' :
+    function (e) {
+        FlowRouter.go('home');
+    }
+})
